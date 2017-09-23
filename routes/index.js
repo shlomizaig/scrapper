6c0cc65e1601ea0377db3993d6991afa9866e758
shlomi szaig <shlomi@convertmedia.com>
2017-09-23 10:14:26 +0300

    adding CDB

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var gplay = require('google-play-scraper');
+//var gplay = require('google-play-scraper');
+var readable = require('constant-db').readable;
+ 
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var gplay = require('google-play-scraper');
-  gplay.app({appId: 'com.dxco.pandavszombies'})
-  .then(function(err){res.json({error:err});}, function(data){res.json(data);});
+  var reqParam = req.query.id || 'com.zynga.livepoker' ;
   
+  var reader = new readable('./cdbfile');
+  reader.open(function cdbOpened(err) {
+    reader.get(reqParam, function gotRecord(err, data) {
+        res.json(data.toString()); // results in 'hello world!' 
+        reader.close();
+    });
+});
+ 
   //res.render('index', { title: 'Express' });
 });
 
